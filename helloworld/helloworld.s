d813965d849e19b398e67e188de2ce2b49f59042
Andrew Taylor <rolyata47@gmail.com>
2025-07-15 12:15:50 -0500

    Adding helloworld.s

---
+++ helloworld/helloworld.s
@@ -0,0 +1,18 @@
+.global main
+.p2align 2
+
+main:
+    mov X0, #1
+    adr X1, str
+    mov X2, len
+    mov X16, #4
+    svc #0
+
+    mov X0, #0
+    mov X16, #1
+    svc #0
+
+str: 
+    .ascii "Hello world!\n"
+
+len = . - str
